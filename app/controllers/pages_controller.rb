a3dedb6c9ab7bf632673e9c26a91cdb5ade3aff0
Matthijs <matthijs_mandjes@hotmail.com>
2017-09-18 15:02:40 +0200

    Add random and name

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
-  
+
   def home
+    @students = Match.where(active: true).count
+    @profile = current_user.profile
   end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,5 @@ class User < ApplicationRecord
 
          def full_name
            profile.full_name
-         end  
+         end
 end
