d1fc94cd7725d739950703370e84e2b4c4535ab8
anapaulalg <ana_paula_lg@hotmail.com>
2017-09-18 11:52:52 +0200

    Create model Day

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918091920) do
+ActiveRecord::Schema.define(version: 20170918094944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "days", force: :cascade do |t|
+    t.date "current_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "matches", force: :cascade do |t|
     t.string "name"
---
+++ spec/factories/days.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :day do
+    current_date "2017-09-18"
+  end
+end
