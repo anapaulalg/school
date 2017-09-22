12847e74825a2a27ffa50299f0477af45b04143d
Matthijs <matthijs_mandjes@hotmail.com>
2017-09-22 09:36:40 +0200

    The algoritm

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,35 +2,33 @@ class PagesController < ApplicationController
 
   def home
     @profile = current_user.profile
-    @student_id = []
+    @count_rows = Profile.where(occupation: "student").count
+    @check = Pair.count
 
-    @profile.active = false
-    @profile.save
+    if @check == 0
+      @round_amount = @count_rows - 1
+      @students = Profile.where(occupation: "student").pluck(:id).to_a
+      @first_student = rand(0..@round_amount)
+      @remove_student = @students[@first_student...@first_student + 1]
 
-    @profiles = Profile.where(active: true)
-    @profiles.each do |profile|
-       @student_id << profile.id
+      @pairs_per_round = @count_rows / 2
+
+      @round_amount.times do |round|
+        @students.delete(@remove_student[0])
+        @students = [@remove_student[0]] + @students
+
+        @pairs_per_round.times do |pair|
+          @pair1 = @students[pair]
+          @pair2 = @students.reverse[pair]
+          Pair.new(user_id: @pair1, pair_user_id: @pair2, pair_at: Time.now.strftime("%Y-%m-%d")).save
+        end
+        @students = @students.rotate(-1)
+
+        #adds first id of the students array after deleting and rotating
+      end
     end
 
-    @current_time = Time.now.strftime("%Y/%m/%d")
-    @pair_id = @student_id.sample(1)
-    @student = Profile.where(id: @pair_id).first
-    @pairs = Pair.where(user_id: @profile.id, pair_user_id: @student.id).first unless !@student
-    @test = Pair.where(user_id: @profile.id, pair_at: @current_time).first
 
-    if(!@pairs && !@test)
-        @student.active = false
-        @student.save
-        @add = Pair.new(user_id: @profile.id, pair_user_id: @student.id, pair_at: Time.now.strftime("%Y/%m/%d"))
-        @add.save
-        @add = Pair.new(user_id: @student.id, pair_user_id: @profile.id, pair_at: Time.now.strftime("%Y/%m/%d"))
-        @add.save
-    end
-      #If current user and match already exist
-    @student = Profile.where(id: @test.pair_user_id).first unless !@test
-  end
-
-
-
+  end #method
 
 end
