65c0f41e5b444d948f1ced8e01af8a0027d57e60
anapaulalg <ana_paula_lg@hotmail.com>
2017-09-22 09:42:22 +0200

    Redirecting Log in

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,10 @@
 class AdminController < ApplicationController
 
   def index
+    if (current_user.profile.occupation != 'admin')
+      return redirect_to index_path
+    end
+
     @profile = current_user.profile
     @profiles = Profile.all
   end
